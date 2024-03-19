16992c7f7cb6f5019265a587ede75bb5bb31c887
hajin-chung <hajinchung1@gmail.com>
2024-03-19 17:03:30 +0900

    more docker

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
+  server: {
+    port: 4000,
+    host: "0.0.0.0",
+  },
+  preview: {
+    port: 4000,
+    strictPort: true,
+  },
   plugins: [react()],
   resolve: {
     alias: {
