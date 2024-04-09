dd4dff0c45e6d27e990c267676a10f357818e534
hajin-chung <hajinchung1@gmail.com>
2024-04-10 05:32:38 +0900

    remove env from docker ignore

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-console.log(import.meta.env)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
